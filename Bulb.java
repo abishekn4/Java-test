ebc80ab718ed4d723aa2e76ad23bb9ea08eb2d4d
abishekn1 <abishekraj0405@gmail.com>
2022-08-20 16:38:30 +0530

    test-code

---
+++ Bulb.java
@@ -0,0 +1,12 @@
+class Bulb{
+
+	String brand;
+	int price;
+	int watts;
+	String color;
+	int warranty;
+
+
+	
+
+}
---
+++ Chrome.java
@@ -0,0 +1,37 @@
+class Chrome{
+	public static void main(String[] args){
+
+		String[] studentList={"ram", "sanjay", "abishek", "karthik"};
+		System.out.println(studentList[1]);
+		System.out.println(studentList[0]);
+
+
+		int[] jersyNo={18, 7, 14 , 17, 5, 10,7,6,9,11,12,13,15,20,21,22,23,24,25,26,27};
+		System.out.println(jersyNo[2]);
+		System.out.println(jersyNo[4]);
+
+
+
+		int total=jersyNo.length-1;
+		System.out.println(jersyNo[total]);
+
+
+			
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
